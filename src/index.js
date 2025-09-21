b68354280eba15c2c04421cbb3958cc13b86c836
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2025-09-21 09:10:47 +0200

    finished changes on html js and css files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-function showAnswer(response) {
-   alert(response.data.answer);
-}
+let button1 = document.querySelector("#challenge-1");
+button1.setAttribute("disabled", "disabled");
 
-const apiKey = "297bdob5643aebcfc422bc019b792eta";
-const context =
-   "You are an expert in history, provide an answer no longer than 40 words.";
-let prompt =
-   "Who was the first Female President or Prime Minister in the world?";
-const apiUrl =
-   "https://api.shecodes.io/ai/v1/generate" +
-   "?prompt=" +
-   encodeURIComponent(prompt) +
-   "&context=" +
-   encodeURIComponent(context) +
-   "&key=" +
-   apiKey;
+let button2 = document.querySelector("#challenge-2");
+button2.style.background = "green";
 
-axios.get(apiUrl).then(showAnswer);
+let button3 = document.querySelector("#challenge-3");
+button3.classList.add("danger");
